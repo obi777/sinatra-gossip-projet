84add129af491d744c672f587465ef25f48d15b0
diwa bitchoka <bdiwa89@gmail.com>
2019-07-29 18:34:54 +0200

    unfinished

---
+++ lib/gossip.rb
@@ -0,0 +1,31 @@
+require 'csv'
+class Gossip
+    
+    require 'csv'
+    attr_reader :author, :content
+
+    def initialize(author, content)
+        @author = author
+        @content = content
+    end
+
+    def save
+        CSV.open("./db/gossip.csv", "ab") do |csv|
+        csv << [@author, @content]
+        end 
+    end
+
+    def self.all
+        all_gossips = []
+        CSV.read("./db/gossip.csv").each do |csv_line|
+          all_gossips << Gossip.new(csv_line[0], csv_line[1])
+        end
+        return all_gossips
+    end
+
+     def self.find(index)
+        self.all[index]
+    end
+
+    
+end
